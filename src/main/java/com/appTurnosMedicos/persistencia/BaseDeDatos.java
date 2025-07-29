8ff6888d9827efa442859f05ff4d5691ca7efe2e
rolagiglia <rodrigolagiglia@gmail.com>
2025-07-29 16:53:43 -0300

    implementa lazy conection a bd y reintentos. soluciona error primera conexion

--- src/main/java/com/appTurnosMedicos/persistencia/BaseDeDatos.java
+++ src/main/java/com/appTurnosMedicos/persistencia/BaseDeDatos.java
@@ -10,19 +10,40 @@ import java.sql.SQLException;
 
 public class BaseDeDatos {
     private static HikariDataSource dataSource;
+    private static boolean poolFallido = false;
 
-    static {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:sqlserver://turnosmedicos.database.windows.net:1433;database=turnosMedicos;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
-        config.setUsername("administradorRodrigo");
-        config.setPassword("elAdminRodri#");
-        config.setMaximumPoolSize(10);
-        config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    private static void inicializar() {
+        if (dataSource == null && !poolFallido) {
+            int intentos = 3;
+            for (int i = 1; i <= intentos; i++) {
+                try {
+                    HikariConfig config = new HikariConfig();
+                    config.setJdbcUrl("jdbc:sqlserver://turnosmedicos.database.windows.net:1433;database=turnosMedicos;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+                    config.setUsername("administradorRodrigo@turnosmedicos");
+                    config.setPassword("elAdminRodri#");
+                    config.setMaximumPoolSize(10);
+                    config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-        dataSource = new HikariDataSource(config);
+                    dataSource = new HikariDataSource(config);
+                    break;
+                } catch (Exception e) {
+                    if (i == intentos) {
+                        poolFallido = true;
+                        System.err.println("❌ No se pudo conectar luego de varios intentos.");
+                    } else {
+                        try {
+                            Thread.sleep(2000); // Espera 5 segundos antes del siguiente intento
+                        } catch (InterruptedException ignored) {}
+                    }
+                }
+            }
+        }
     }
 
     public static Connection obtenerConexion() throws SQLException {
+        inicializar();
+        if(dataSource == null)
+            throw new SQLException("No se pudo establecer conexión con la base de datos. Por favor, intente más tarde.");
         return dataSource.getConnection();
     }
     public static void cerrarPool() {
